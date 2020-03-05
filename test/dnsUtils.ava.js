375f26e2457249722e50f59c0e2152e277d12837
Joe Hildebrand <joe-github@cursive.net>
2020-03-05 13:38:50 -0700

    Fix hexdump.  Add verbose logging of certs for HTTPs.

--- test/dnsUtils.ava.js
+++ test/dnsUtils.ava.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const stream = require('stream')
 const test = require('ava')
 const DNSutils = require('../lib/dnsUtils')
 const packet = require('dns-packet')
@@ -35,4 +36,66 @@ test('normalizeArgs', t => {
 
 test('base64urlEncode', t => {
   t.is(DNSutils.base64urlEncode(Buffer.from('fbff', 'hex')), '-_8')
+})
+
+test('hexDump', t => {
+  const s = new stream.Transform({
+    encoding: 'utf8',
+    transform (chunk, enc, cb) {
+      cb(null, chunk)
+    }
+  })
+  for (const sz of [0, 1, 4, 8, 12, 16, 24, 32]) {
+    DNSutils.hexDump(Buffer.alloc(sz), s)
+  }
+  t.is(s.read(), `\
+00000000
+00000000  00                                                |.|
+00000001
+00000000  00 00 00 00                                       |....|
+00000004
+00000000  00 00 00 00 00 00 00 00                           |........|
+00000008
+00000000  00 00 00 00 00 00 00 00  00 00 00 00              |............|
+0000000c
+00000000  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00  |................|
+00000010
+00000000  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00  |................|
+00000010  00 00 00 00 00 00 00 00                           |........|
+00000018
+00000000  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00  |................|
+00000010  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00  |................|
+00000020
+`)
+  const tt = new stream.Transform({
+    transform (chunk, enc, cb) {
+      cb(null, chunk)
+    }
+  })
+  tt.isTTY = true
+  DNSutils.hexDump(Buffer.from('666f6f7fa2adae'), tt)
+  t.deepEqual(tt.read(), Buffer.from(`\
+1b5b39306d30303030303030301b5b33396d2020333620333620333620363620\
+3336203636203337203636202036312033322036312036342036312036352020\
+2020202020207c1b5b33326d361b5b33396d1b5b33326d361b5b33396d1b5b33\
+326d361b5b33396d1b5b33326d661b5b33396d1b5b33326d361b5b33396d1b5b\
+33326d661b5b33396d1b5b33326d371b5b33396d1b5b33326d661b5b33396d1b\
+5b33326d611b5b33396d1b5b33326d321b5b33396d1b5b33326d611b5b33396d\
+1b5b33326d641b5b33396d1b5b33326d611b5b33396d1b5b33326d651b5b3339\
+6d7c0a1b5b39306d30303030303030651b5b33396d0a`,'hex'))
+})
+
+test('buffersToB64', t => {
+  const a = {
+    b: Buffer.alloc(2),
+    d: [Buffer.alloc(2)],
+    e: null
+  }
+  a.c = a
+  t.deepEqual(DNSutils.buffersToB64(a), {
+    b: 'AAA=',
+    d: ['AAA='],
+    e: null,
+    c: '[Circular reference]'
+  })
 })
